b05144ee462abe29e61324f3a8f405651d60fc7f
CeliaDawn <mynameisaverb1@gmail.com>
2025-02-05 13:22:49 -0500

    ever grande done

--- songs.mk
+++ songs.mk
@@ -1342,7 +1342,7 @@ $(MID_SUBDIR)/mus_vs_legendary2.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G196 -V100
 
 $(MID_SUBDIR)/mus_vs_gym_leader2.s: %.s: %.mid
-	$(MID) $< $@ -E -R$(STD_REVERB) -G196 -V100
+	$(MID) $< $@ -E -R$(STD_REVERB) -G197 -V100
 
 $(MID_SUBDIR)/mus_ever_grande_road.s: %.s: %.mid
-	$(MID) $< $@ -E -R$(STD_REVERB) -G196 -V100
+	$(MID) $< $@ -E -R$(STD_REVERB) -G196 -V115
