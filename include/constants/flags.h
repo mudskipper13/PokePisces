2db1ca3eb12dcafc5fdcde1a4b956a649d395ef4
Chairry <chairvaria@gmail.com>
2025-02-24 22:47:15 -0500

    Attempted fix for cold mines bugs reported by TrainerX in january

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1278,7 +1278,7 @@
 #define FLAG_SHAYON_ORB                                           0x4CC // Unused Flag
 #define FLAG_LUOSHAN_ORB                                           0x4CD // Unused Flag
 #define FLAG_DEFEATED_GROTTO_SKELLYWARG                                           0x4CE // Unused Flag
-#define FLAG_UNUSED_0x4CF                                           0x4CF // Unused Flag
+#define FLAG_ACTIVATED_COLD_MINES_ELECTRICIAN                                           0x4CF // Unused Flag
 #define FLAG_UNUSED_0x4D0                                           0x4D0 // Unused Flag
 #define FLAG_UNUSED_0x4D1                                           0x4D1 // Unused Flag
 #define FLAG_UNUSED_0x4D2                                           0x4D2 // Unused Flag
